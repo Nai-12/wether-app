fe2fad490f604d37945ae2e7c04cc1823f8d560d
Nai-12 <nabail349@gmail.com>
2026-09-16 13:53:41 +0700

    refactor(logo): mengembalikan logo bmkg ke footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import WeatherInfo from "@/components/weatherInfo";
+import Image from "next/image";
 // import Loading from "./loading";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <>
       {/* <Loading /> */}
       <WeatherInfo />
-      <footer className="font-pop fixed bottom-0 left-0 right-0 flex justify-center items-center py-2 gap-10 bg-[#222222] text-white font-light text-[10px]">
+      <footer className="font-pop fixed bottom-0 left-0 right-0 flex justify-center items-center py-2 gap-3 bg-[#222222] text-white font-light text-[10px]">
         <p>
           Created With 💖 by Nabil Putra, Github :{" "}
           <a
@@ -16,6 +17,10 @@ export default function Home() {
             Nai-12
           </a>
         </p>
+        <div className="flex justify-center items-center gap-2">
+          <Image src="/bmkg.svg" alt="bmkg image" width={30} height={30} />
+          <h1 className="font-pop font-bold">BMKG</h1>
+        </div>
       </footer>
     </>
   );
