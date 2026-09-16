761c0f465599d5137fd8fbf80eb02365261cccba
Nai-12 <nabail349@gmail.com>
2026-09-16 14:01:16 +0700

    refactor(footer): mengganti kalimat footer agar lebih ringkas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
       <WeatherInfo />
       <footer className="font-pop fixed bottom-0 left-0 right-0 flex justify-center items-center py-2 gap-3 bg-[#222222] text-white font-light text-[10px]">
         <p>
-          Created With 💖 by Nabil Putra, Github :{" "}
+          Github :{" "}
           <a
             href="https://github.com/Nai-12/weather-info"
             className="underline"
