68dc3b08283ef445f101630d6a1354672c2c7ea8
Nai-12 <nabail349@gmail.com>
2026-09-24 11:43:29 +0700

    refactor(footer): mengubah link ke github dengan link yang sesuai

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,10 +10,7 @@ export default function Home() {
       <footer className="font-pop fixed bottom-0 left-0 right-0 flex justify-center items-center py-2 gap-3 bg-[#222222] text-white font-light text-[10px]">
         <p>
           Github :{" "}
-          <a
-            href="https://github.com/Nai-12/weather-info"
-            className="underline"
-          >
+          <a href="https://github.com/Nai-12/wether-app" className="underline">
             Nai-12
           </a>
         </p>
