6aa8764a0903f31ade36554b378c08a995bf005c
Nai-12 <nabail349@gmail.com>
2026-09-25 05:33:07 +0700

    fix(ui): memperbaiki bug ui responsive di bagian error.jsx

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,9 +1,11 @@
 export default function NotFound() {
   return (
     <>
-      <div className="fixed top-0 bottom-0 flex justify-center items-center flex-col">
-        <h1 className="text-9xl font-mont font-bold text-amber-50">MAAF!</h1>
-        <p className="font-pop font-semibold text bg-amber-50 px-5 rounded-sm text-center">
+      <div className="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center flex-col">
+        <h1 className="text-8xl font-mont font-bold text-amber-50 lg:text-9xl">
+          MAAF!
+        </h1>
+        <p className="font-pop font-semibold text bg-amber-50 px-5 rounded-sm text-center text-sm">
           Area yang anda masukan tidak terdaftar
         </p>
       </div>
