8b01e2f53f2042079a550365623a20af736afade
Nai-12 <nabail349@gmail.com>
2026-09-25 21:14:48 +0700

    feat(security): menambahkan headers di config untuk keamanan sementara

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.natzserver.my.id/",
+          },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), payment=(), usb=(), fullscreen=()",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
