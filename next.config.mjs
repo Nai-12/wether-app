ae1823b1d006a75f76d84bb1f1815acb13210691
Nai-12 <nabail349@gmail.com>
2026-09-26 14:49:47 +0700

    refactor(config): perubahan pada next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,7 @@ const nextConfig = {
             value: "https://www.natzserver.my.id/",
           },
           { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          {
-            key: "Permissions-Policy",
-            value:
-              "camera=(), microphone=(), payment=(), usb=(), fullscreen=()",
-          },
         ],
       },
     ];
